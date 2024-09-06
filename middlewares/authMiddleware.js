a0a1e27bfe7eb724054c7825320dc347afbf09eb
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-09-07 00:06:58 +0200

    error corrected

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ const generateToken = (user) => {
     return jwt.sign({user:user.id}, {secret, hashedSecret},{expireIn: '1h'})
 }
 
-const verifyToken (req,res,next) {
+const verifyToken = (req,res,next) => {
     const token = req.session.token;
     if(!token) {
         return res.status(401).json({message: 'Token not provided'})
